b0501e227e752a920a55d43a4280c22878aafd1d
Yuchen He <yuchenhe@Yuchens-MacBook-Pro.local>
2019-02-06 13:07:14 +0100

    refactor code

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,154 +1,196 @@
-/*
-import TSim.*;
+import java.util.concurrent.Semaphore;
 
-public class Lab1 {
-
-    public Lab1(int speed1, int speed2) {
-        
-        TSimInterface tsi = TSimInterface.getInstance();
-
-        try {
-            tsi.setSpeed(1,speed1);
-            tsi.setSpeed(2,speed2);
-            try {
-                System.out.println(tsi.getSensor(1).getStatus());
-            }
-            catch (InterruptedException e) {
-                e.printStackTrace();    // or only e.getMessage() for the error
-                System.exit(1);
-            }
-        }
-        catch (CommandException e) {
-            e.printStackTrace();    // or only e.getMessage() for the error
-            System.exit(1);
-        }
-        
-    }
-} */
-
-
-//import TSim.AddingArrayList;
 import TSim.CommandException;
 import TSim.SensorEvent;
 import TSim.TSimInterface;
-import java.util.concurrent.Semaphore;
-
 
 public class Lab1 {
+    
+    final static int DOWNWARDS = 1;
+    final static int UPWARDS = 1;
 
-  private TSimInterface tsi;
-  //private AddingArrayList<Semaphore> semaphores;
+    private TSimInterface tsi;
 
-  public Lab1(Integer speed1, Integer speed2) {
-    tsi = TSimInterface.getInstance();
-    //semaphores = new AddingArrayList<>();
+    //private AddingArrayList<Semaphore> semaphores;
+    Semaphore upperLoopControlLeft, upperLoopControlRight, lowerLoopControlLeft, lowerLoopControlRight;
+    
+    int[][] sensors;
+    int[][] switches;
 
-    Thread train1 = new Train(1, speed1, true);
-    Thread train2 = new Train(2, speed2, false);
+    public Lab1(int firstSpeed, int SecondSpeed) {
+        
+        tsi = TSimInterface.getInstance();
+                
+        upperLoopControlLeft = new Semaphore(1);
+        upperLoopControlRight = new Semaphore(1);
+        lowerLoopControlLeft = new Semaphore(1);
+        lowerLoopControlRight = new Semaphore(1);
+        
+        initialiseSensors();
 
-    train1.start();
-    train2.start();
-  }
+        Thread train1 = new Train(1, firstSpeed, true);
+        Thread train2 = new Train(2, SecondSpeed, false);
 
-  public class Train extends Thread {
-
-    int id;
-    int speed;
-    boolean downward;
-
-    public Train(int id, int speed, boolean downward) {
-      this.id = id;
-      this.speed = speed;
-      this.downward = downward;
+        train1.start();
+        train2.start();
+    }
+    
+    private void initialiseSensors() {
+        
+        int[][] newSensors = {
+            {1, 11}, //0                  
+            {2, 9}, //1
+            {3, 13}, //2
+            {5, 11}, //3
+            {6, 7}, //4
+            {6, 9}, //5
+            {6, 10}, //6
+            {10, 7}, //7
+            {10, 8}, //8
+            {12, 9}, //9
+            {13, 10}, //10
+            {14, 7}, //11
+            {15, 3}, //12
+            {15, 5}, //13
+            {15, 8}, //14
+            {15, 11}, //15
+            {15, 13}, //16
+            {19, 9} //17
+        };
+        
+        sensors = newSensors;
     }
 
-    @Override
-    public void run() {
-      try {
+    public class Train extends Thread {
 
-        tsi.setSpeed(id, speed);///Speed args are 15 and 15.
-        //tsi.setSpeed(id, 0); // Prevent the speed to become 0 when reaching a station
+        int id;
+        int speed;
+        int maxSpeed;
+        boolean downward;
 
-        while (true) {
-          SensorEvent se = tsi.getSensor(id);
-          //Coming from north
-
-          if ((se.getStatus() == SensorEvent.ACTIVE)) {
-            if ((se.getXpos() == 14) && (se.getYpos() == 7) && downward) {
-              tsi.setSwitch(17, 7, TSimInterface.SWITCH_RIGHT);
-            }
-
-            if ((se.getXpos() == 19) && (se.getYpos() == 9) && downward) {
-              tsi.setSwitch(15, 9, TSimInterface.SWITCH_RIGHT);
-            }
-
-            if ((se.getXpos() == 6) && (se.getYpos() == 9) && downward) {
-              tsi.setSwitch(4, 9, TSimInterface.SWITCH_LEFT);
-            }
-
-            if ((se.getXpos() == 15) && (se.getYpos() == 8) && downward) {
-              tsi.setSwitch(17, 7, TSimInterface.SWITCH_LEFT);
-            }
-
-            if ((se.getXpos() == 1) && (se.getYpos() == 10) && downward) {
-              tsi.setSwitch(3, 11, TSimInterface.SWITCH_RIGHT);
-            }
-
-            //Coming from south
-            if ((se.getXpos() == 1) && (se.getYpos() == 10) && !downward) {
-              tsi.setSwitch(4, 9, TSimInterface.SWITCH_RIGHT);
-            }
-
-            if ((se.getXpos() == 13) && (se.getYpos() == 10) && !downward) {
-              tsi.setSwitch(15, 9, TSimInterface.SWITCH_LEFT);
-            }
-
-            if ((se.getXpos() == 5) && (se.getYpos() == 11) && !downward) {
-              tsi.setSwitch(3, 11, TSimInterface.SWITCH_LEFT);
-            }
-
-            if ((se.getXpos() == 19) && (se.getYpos() == 9) && !downward) {
-              tsi.setSwitch(17, 7, TSimInterface.SWITCH_LEFT);
-            }
-
-            if ((se.getXpos() == 4) && (se.getYpos() == 13) && !downward) {
-              tsi.setSwitch(3, 11, TSimInterface.SWITCH_RIGHT);
-            }
-
-            if ((se.getXpos() == 12) && (se.getYpos() == 9) && !downward) {
-              tsi.setSwitch(15, 9, TSimInterface.SWITCH_RIGHT);
-            }
-
-            //North station
-            if ((se.getXpos() == 15) && ((se.getYpos() == 5) || se.getYpos() == 3)) {
-              if (se.getStatus() == SensorEvent.ACTIVE) {
-                tsi.setSpeed(se.getTrainId(), 0);
-                downward = true;
-                sleep(1000 + Math.abs(speed) * 15);
-                speed = -speed;
-                tsi.setSpeed(se.getTrainId(), speed);
-              }
-            }
-
-            //South station
-            if ((se.getXpos() == 15) && ((se.getYpos() == 11) || se.getYpos() == 13)) {
-              if (se.getStatus() == SensorEvent.ACTIVE) {
-                tsi.setSpeed(se.getTrainId(), 0);
-                downward = false;
-                sleep(1000 + Math.abs(speed) * 15);
-                speed = -speed;
-                tsi.setSpeed(se.getTrainId(), speed);
-              }
-            }
-          }
+        public Train(int id, int speed, boolean downward) {
+            this.id = id;
+            this.speed = speed;
+            maxSpeed = 16;
+            this.downward = downward;
         }
-      } catch (CommandException e) {
-        e.printStackTrace();
-        System.exit(1);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-        System.exit(1);
-      }
+        
+        private boolean isSensorDetection(SensorEvent se, int index) {
+            if (se.getXpos() == sensors[index][0] && se.getYpos() == sensors[index][1]) {
+                return true;
+            }
+            
+            return false;
+        }
+        
+        @Override
+        public void run() {
+            try {
+
+                tsi.setSpeed(id, speed);///Speed args are 15 and 15.
+                //tsi.setSpeed(id, 0); // Prevent the speed to become 0 when reaching a station
+                if (speed < maxSpeed) {
+                    speed++;
+                }
+                
+                while (true) {
+                    SensorEvent se = tsi.getSensor(id);
+                    
+
+                    if ((se.getStatus() == SensorEvent.ACTIVE)) {
+                        
+                        
+                        /* Downwards detections. */
+                        
+                        // sensor: 14, 7; switch: 17, 7
+                        if (isSensorDetection(se, 11) && downward) {
+                            tsi.setSwitch(17, 7, TSimInterface.SWITCH_RIGHT);
+                        }
+                        
+                        // sensor: 19, 9; switch: 15, 9
+                        if (isSensorDetection(se, 17) && downward) {
+                            tsi.setSwitch(15, 9, TSimInterface.SWITCH_RIGHT);
+                        }
+                        
+                        // sensor: 6, 9; switch: 4, 9
+                        if (isSensorDetection(se, 5) && downward) {
+                            tsi.setSwitch(4, 9, TSimInterface.SWITCH_LEFT);
+                        }
+                        
+                        // sensor: 15, 8; switch: 17, 7
+                        if (isSensorDetection(se, 14) && downward) {
+                            tsi.setSwitch(17, 7, TSimInterface.SWITCH_LEFT);
+                        }
+                        
+                        // sensor: 1, 10; switch: 3, 11 
+                        if (isSensorDetection(se, 0) && downward) {
+                            tsi.setSwitch(3, 11, TSimInterface.SWITCH_RIGHT);
+                        }
+
+                        /* Upwards detections. */
+                        
+                        // sensor: 1, 10; switch: 4, 9
+                        if (isSensorDetection(se, 0) && !downward) {
+                            tsi.setSwitch(4, 9, TSimInterface.SWITCH_RIGHT);
+                        }
+
+                        // sensor: 13, 10; switch: 15, 9
+                        if (isSensorDetection(se, 10) && !downward) {
+                            tsi.setSwitch(15, 9, TSimInterface.SWITCH_LEFT);
+                        }
+
+                        // sensor: 5, 11; switch: 3, 11
+                        if (isSensorDetection(se, 3) && !downward) {
+                            tsi.setSwitch(3, 11, TSimInterface.SWITCH_LEFT);
+                        }
+
+                        // sensor: 19, 9; switch: 17, 7
+                        if (isSensorDetection(se, 17) && !downward) {
+                            tsi.setSwitch(17, 7, TSimInterface.SWITCH_LEFT);
+                        }
+                        
+                        // sensor: 3, 13; switch: 3, 11
+                        if (isSensorDetection(se, 2) && !downward) {
+                            tsi.setSwitch(3, 11, TSimInterface.SWITCH_RIGHT);
+                        }
+
+                        // sensor: 12, 9; switch: 15, 9
+                        if (isSensorDetection(se, 9) && !downward) {
+                            tsi.setSwitch(15, 9, TSimInterface.SWITCH_RIGHT);
+                        }
+
+                        //North station
+                        if ((se.getXpos() == 15) && ((se.getYpos() == 5) || se.getYpos() == 3)) {
+                            if (se.getStatus() == SensorEvent.ACTIVE) {
+                                tsi.setSpeed(se.getTrainId(), 0);
+                                downward = true;
+                                sleep(1000 + Math.abs(speed) * 15);
+                                speed = -speed;
+                                tsi.setSpeed(se.getTrainId(), speed);
+                            }
+                        }
+
+                        //South station
+                        if ((se.getXpos() == 15) && ((se.getYpos() == 11) || se.getYpos() == 13)){
+                            if (se.getStatus() == SensorEvent.ACTIVE) {
+                                tsi.setSpeed(se.getTrainId(), 0);
+                                downward = false;
+                                sleep(1000 + Math.abs(speed) * 15);
+                                speed = -speed;
+                                tsi.setSpeed(se.getTrainId(), speed);
+                            }
+                        }        
+                    }
+                }
+                            
+            } catch (CommandException e) {
+                e.printStackTrace();
+                System.exit(1);
+                
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
     }
-  }
 }
